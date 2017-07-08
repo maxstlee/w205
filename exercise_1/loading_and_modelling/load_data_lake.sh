62e3d5a8a3ac112331eaedbab539b49de74335fe
w205 <w205@ip-172-31-23-142.ec2.internal>
2017-07-08 21:59:49 +0000

    files

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -31,12 +31,20 @@ file5="hvbp_hcahps_11_10_2016.csv"
 new5="surveys_responses.csv"
 tail -n +2 $file5 >$new5
 
+
 hdfs dfs -mkdir /user/w205/hospital_compare
-hdfs dfs -put $new1 /user/w205/hospital_compare
-hdfs dfs -put $new2 /user/w205/hospital_compare
-hdfs dfs -put $new3 /user/w205/hospital_compare
-hdfs dfs -put $new4 /user/w205/hospital_compare
-hdfs dfs -put $new5 /user/w205/hospital_compare
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/measures
+hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
+
+hdfs dfs -put $new1 /user/w205/hospital_compare/hospitals
+hdfs dfs -put $new2 /user/w205/hospital_compare/effective_care
+hdfs dfs -put $new3 /user/w205/hospital_compare/readmissions
+hdfs dfs -put $new4 /user/w205/hospital_compare/measures
+hdfs dfs -put $new5 /user/w205/hospital_compare/survey_responses
+
 
 cd $dir
 
