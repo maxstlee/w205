a7e2d671ee5393532f892737335090f5b29c5fcb
maxstlee <maxstlee@gmail.com>
2017-07-09 13:24:24 -0400

    Update hospitals_and_patients.sql

--- exercise_1/Investigations/hospitals_and_patients/hospitals_and_patients.sql
+++ exercise_1/Investigations/hospitals_and_patients/hospitals_and_patients.sql
@@ -1,1 +1,8 @@
+DROP TABLE corrtable;
+CREATE TABLE corrtable AS
+SELECT hp.hospital_name, hp.hospital_overall_rating as rating, CAST(ss.hcahps_base_score AS FLOAT) as base
+FROM hospitalstateandrating AS hp
+INNER JOIN surveys_responses AS ss
+ ON hp.hospital_name = ss.hospital_name;
 
+SELECT CORR(rating, base) FROM corrtable;
