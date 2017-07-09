819b8f24fa0df32e032cbf24e1ce6f4dd6f718ef
maxstlee <maxstlee@gmail.com>
2017-07-09 11:47:38 -0400

    Update best_states.sql

--- exercise_1/Investigations/best_states/best_states.sql
+++ exercise_1/Investigations/best_states/best_states.sql
@@ -1,6 +1,8 @@
 DROP TABLE state_score;
 CREATE TABLE state_score AS
 SELECT state, AVG(hospital_overall_rating) AS avg_rating
-FROM hospitalstateandrating
+FROM hospitalStateandRating
 GROUP BY state
-ORDER BY avg_rating;
+ORDER BY avg_rating DESC;
+
+SELECT * FROM state_score Limit 10;
