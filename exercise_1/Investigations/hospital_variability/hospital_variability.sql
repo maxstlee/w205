a06167a5c21f2f7c3c4fdb7b5373e8e14ace3e6e
maxstlee <maxstlee@gmail.com>
2017-07-09 13:09:10 -0400

    Update hospital_variability.sql

--- exercise_1/Investigations/hospital_variability/hospital_variability.sql
+++ exercise_1/Investigations/hospital_variability/hospital_variability.sql
@@ -1,1 +1,16 @@
+DROP TABLE procvar;
+CREATE TABLE procvar AS
+SELECT measure_id, VARIANCE(Score) AS var
+FROM procedurescores
+GROUP BY measure_id
+ORDER BY var DESC
+LIMIT 10;
 
+DROP TABLE finalvar;
+CREATE TABLE finalVAR AS
+SELECT pv.measure_id, measure.measure_name, pv.var
+FROM procvar AS pv
+INNER JOIN measurecompact AS measure
+  ON pv.measure_id = measure.measure_id;
+  
+SELECT * from finalvar;
